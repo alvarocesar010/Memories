84364b3853dffd188338188ebb3d5c1e703207d7
Alvaro Souza <alvarocesar010@hotmail.com>
2025-07-23 18:09:11 +0100

    Add base files and folders for the Memories application

---
+++ backend/controllers/MemoryController.js
@@ -0,0 +1,10 @@
+const Memory = require("../models/Memory");
+
+
+const createMemory = async (req, res) => {
+  res.json("Deu Certo");
+};
+
+module.exports = {
+  createMemory,
+};
---
+++ backend/models/Memory.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const MemorySchema = new Schema(
+  {
+    tittle: {
+      type: String,
+      required: true,
+    },
+    src: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      reqired: true,
+    },
+    favorite: {
+      type: Boolean,
+    },
+    comments: [commentSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Memory", MemorySchema)
