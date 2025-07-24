2e8acec9a6db75b542e47b6c82947782d5c7b134
Alvaro Souza <alvarocesar010@hotmail.com>
2025-07-24 23:36:22 +0100

    feat: AddMemory.jsx implementation axios config, HTML structure and CSS

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,10 +1,30 @@
+import axios from "axios"
+
+import { useState } from "react"
+
+import './AddMemory.css'
 
 const AddMemory = () => {
-  return (
-    <div>
-      <h1>Add Memory</h1>
-    </div>
-  )
+    return (
+        <div className="add-memory-page">
+            <h2>Create a new memory</h2>
+            <form >
+                <label>
+                    <p>Title:</p>
+                    <input type="text" placeholder="Define a title" name="title" />
+                </label>
+                <label>
+                    <p>Description:</p>
+                    <textarea type="text" placeholder="Describe about the memory..." name="description" />
+                </label>
+                <label>
+                    <p>Image:</p>
+                    <input type="file" name="image" />
+                </label>
+                <input type="submit" value="Send"  className="btn"/>
+            </form>
+        </div>
+    )
 }
 
 export default AddMemory
